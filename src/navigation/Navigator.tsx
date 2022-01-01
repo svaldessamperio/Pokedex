c05fd8981d68e248390d5c78532db1fffd352ce9
svaldessamperio <svaldes_samperio@hotmail.com>
2022-01-01 13:50:23 -0600

    Se instalan componentes nuevamente

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import PokemonScreen from '../screens/PokemonScreen';
