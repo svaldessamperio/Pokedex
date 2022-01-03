0ced63a61a2ef6f290751262a1e26d1dd4cdcd50
svaldessamperio <svaldes_samperio@hotmail.com>
2022-01-02 20:19:32 -0600

    HomeScreen inicio e inclusión de axios

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { styles } from '../theme/Theme';
 
 export default function HomeScreen() {
+    const { top } = useSafeAreaInsets();
+
     return (
-        <View>
-            <Text>Home Screen ....</Text>
-        </View>
+        <>
+            <Image 
+                source={ require('../assets/pokebola.png') }
+                style={styles.pokebolaBG}
+            />
+
+            <Text
+                style={[
+                    styles.title, 
+                    styles.globalMargin,
+                    {top: top + 20},
+                ]}
+            >
+                Pokedex
+            </Text>
+
+        </>
     )
 }
---
+++ src/theme/Theme.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles= StyleSheet.create({
+    globalMargin:{
+        marginHorizontal:20,
+    },
+    pokebolaBG:{
+        position: 'absolute',
+        height:300,
+        width: 300,
+        top: -100,
+        right: -100,
+        opacity: 0.2,
+    },
+    title: {
+        fontSize:35,
+        fontWeight: 'bold',
+    },
+})
